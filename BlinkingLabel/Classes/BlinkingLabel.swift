7c812283d98bd34ad11a49c512cf6c3c5b6f6a75
Peter Todd <peter@petertodd.com>
2016-06-21 10:41:37 +0100

    Working example swift cocoa pod

---
+++ BlinkingLabel/Classes/BlinkingLabel.swift
@@ -0,0 +1,23 @@
+//
+//  BlinkingLabel.swift
+//  Pods
+//
+//  Created by Peter Todd Air on 21/06/2016.
+//
+//
+
+//import Foundation
+
+public class BlinkingLabel : UILabel {
+    public func startBlinking() {
+        let options : UIViewAnimationOptions = [.Repeat , .Autoreverse]
+        UIView.animateWithDuration(0.25, delay:0.0, options:options, animations: {
+            self.alpha = 0
+            }, completion: nil)
+    }
+    
+    public func stopBlinking() {
+        alpha = 1
+        layer.removeAllAnimations()
+    }
+}
